e86b0e510fb473a899b5f3046b0667e1eed6fae4
narendrahl <narendra.llimbu@gmail.com>
2015-01-07 23:15:56 +0530

    Create Python_Data-Logger.py

---
+++ Python_Data-Logger.py
@@ -0,0 +1,135 @@
+# ---  Created From Python_Data-Logger.ui  
+# ---  Using pyuic -----
+#
+# 
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(741, 380)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("naren.ico")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Form.setWindowIcon(icon)
+        self.lineEdit = QtGui.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(290, 30, 81, 21))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.lineEdit_2 = QtGui.QLineEdit(Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(290, 80, 81, 20))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.lineEdit_3 = QtGui.QLineEdit(Form)
+        self.lineEdit_3.setGeometry(QtCore.QRect(290, 150, 113, 20))
+        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+        self.lineEdit_4 = QtGui.QLineEdit(Form)
+        self.lineEdit_4.setGeometry(QtCore.QRect(290, 230, 113, 20))
+        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        self.label = QtGui.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(20, 30, 91, 16))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(40, 82, 46, 21))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_3 = QtGui.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(10, 150, 171, 16))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.label_4 = QtGui.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(10, 230, 261, 16))
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.radioButton = QtGui.QRadioButton(Form)
+        self.radioButton.setGeometry(QtCore.QRect(510, 130, 82, 17))
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
+        self.radioButton_2 = QtGui.QRadioButton(Form)
+        self.radioButton_2.setGeometry(QtCore.QRect(510, 70, 82, 17))
+        self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
+        self.radioButton_3 = QtGui.QRadioButton(Form)
+        self.radioButton_3.setGeometry(QtCore.QRect(510, 150, 82, 17))
+        self.radioButton_3.setObjectName(_fromUtf8("radioButton_3"))
+        self.label_5 = QtGui.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(510, 20, 141, 16))
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.pushButton = QtGui.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(510, 210, 141, 71))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.lineEdit_5 = QtGui.QLineEdit(Form)
+        self.lineEdit_5.setGeometry(QtCore.QRect(10, 300, 381, 51))
+        self.lineEdit_5.setObjectName(_fromUtf8("lineEdit_5"))
+        self.label_6 = QtGui.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(140, 360, 111, 16))
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+        self.label_7 = QtGui.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(450, 320, 251, 20))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_7.setFont(font)
+        self.label_7.setText(_fromUtf8(""))
+        self.label_7.setObjectName(_fromUtf8("label_7"))
+        self.pushButton_2 = QtGui.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(514, 320, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.radioButton_4 = QtGui.QRadioButton(Form)
+        self.radioButton_4.setGeometry(QtCore.QRect(510, 90, 82, 17))
+        self.radioButton_4.setObjectName(_fromUtf8("radioButton_4"))
+        self.radioButton_5 = QtGui.QRadioButton(Form)
+        self.radioButton_5.setGeometry(QtCore.QRect(510, 110, 82, 17))
+        self.radioButton_5.setObjectName(_fromUtf8("radioButton_5"))
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "DATA RECORDER", None))
+        self.label.setText(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt; font-weight:600;\">VARIABLES</span></p></body></html>", None))
+        self.label_2.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">PORT</span></p></body></html>", None))
+        self.label_3.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">NUMBER OF SAMPLES</span></p></body></html>", None))
+        self.label_4.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">SAMPLING INTERVAL ( Seconds )</span></p></body></html>", None))
+        self.radioButton.setText(_translate("Form", "57600", None))
+        self.radioButton_2.setText(_translate("Form", "9600", None))
+        self.radioButton_3.setText(_translate("Form", "115200", None))
+        self.label_5.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">SELECT BAUDRATE</span></p></body></html>", None))
+        self.pushButton.setText(_translate("Form", "START RECORDING", None))
+        self.label_6.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">USB  DATA</span></p></body></html>", None))
+        self.pushButton_2.setText(_translate("Form", "RESET EEPROM", None))
+        self.radioButton_4.setText(_translate("Form", "19200", None))
+        self.radioButton_5.setText(_translate("Form", "38400", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
